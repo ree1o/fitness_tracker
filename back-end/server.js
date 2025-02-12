0a3e08b12ab987ae47f7feac8ea8714b9f8b6260
ree1o <runarkenz@gmail.com>
2025-02-12 15:44:15 +0500

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -9,11 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DB_URL,
 });
 
 app.use(express.json());
