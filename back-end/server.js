7fe9d5c2e7f499c35753c60b6e960901cd51b259
ree1o <runarkenz@gmail.com>
2025-02-13 19:55:13 +0500

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -213,5 +213,7 @@ app.get("/api/workouts", authenticateToken, async (req, res) => {
 });
 
 
-app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Server running on port ${PORT}`);
 });
+
